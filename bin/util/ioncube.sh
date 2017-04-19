33e6067cfd94986763bf0b1ec1d7b83ef581ac03
rangamadala1 <lalitheeee@gmial.com>
2017-04-20 05:03:06 +0530

    ioncube cmd

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -15,3 +15,15 @@ dep_dirname=ioncube
 
 echo "---lalith ioncube installing--> Packaging ext/ioncube ${DEFAULT_VERSION}..."
 
+curl -L ${dep_url} | tar xz
+
+pushd ${dep_dirname}
+ext_dir=/app/.heroku/php/lib/php/extensions/no-debug-non-zts-20131226
+echo ext_dir
+bin_dir=${OUT_PREFIX}/bin
+mkdir -p ${ext_dir}
+mkdir -p ${bin_dir}
+cp ioncube_loader_lin_5.6.so ${ext_dir}/ioncube_loader_lin_5.6.so
+popd
+
+echo "-----> Done."
